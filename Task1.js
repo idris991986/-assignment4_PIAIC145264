9930160a94319e4d0a1a8aa8a66d649e7e4cbeab
Muhammad Idrees <m.idris991986@gmail.com>
2021-01-16 06:03:33 -0800

    Completed Task 1 of Javascript Assignment 1

---
+++ Task1.js
@@ -0,0 +1,5 @@
+var city = "Hyderabad";
+document.write("City: ",city);
+var replaceCity = city.replace("Hyder","Islam");
+document.write("<br>"); document.write("<br>");
+document.write("After replacement: ",replaceCity);
