914f7a52815f0f80fb904bde7814d07fe5590163
Muhammad Idrees <m.idris991986@gmail.com>
2021-01-16 06:20:53 -0800

    Completed Task 2 of Javascript Assignment 4

---
+++ Task2.js
@@ -0,0 +1,9 @@
+var stringType = "472";
+document.write("Value: ",stringType);
+document.write("<br>");
+document.write("Type: ",typeof(stringType));
+document.write("<br>");
+var numberType = Number(stringType);
+document.write("Value: ",numberType);
+document.write("<br>");
+document.write("Type: ",typeof(numberType));
