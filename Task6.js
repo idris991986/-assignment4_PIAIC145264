24ac14813744710cec09ca72ffac023074a77089
Muhammad Idrees <m.idris991986@gmail.com>
2021-01-20 09:52:38 -0800

    Completed Task 6 of Javascript Assignment 4

---
+++ Task6.js
@@ -0,0 +1,7 @@
+var dice = Math.random();
+var diceValue = dice*6;
+var dice1 = Math.random();
+var diceValue1 = dice1*6;
+document.write("random dice value: "+Math.ceil(diceValue));
+document.write("<br>");
+document.write("random dice value: "+Math.ceil(diceValue1));
