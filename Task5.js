06a693c70bf2aa1fc1a8e45641bd05e7252f0b0c
Muhammad Idrees <m.idris991986@gmail.com>
2021-01-20 07:38:03 -0800

    Completed Task 5 of Javascript Assignment 4

---
+++ Task5.js
@@ -0,0 +1,9 @@
+var postiveInteger = prompt("Please enter a number","3.45214");
+postiveInteger = Number(postiveInteger);
+document.write("number: ",postiveInteger);
+document.write("<br>");
+document.write("round off: ",Math.round(postiveInteger));
+document.write("<br>");
+document.write("floor value: ",Math.floor(postiveInteger));
+document.write("<br>");
+document.write("ceil value: ",Math.ceil(postiveInteger));
