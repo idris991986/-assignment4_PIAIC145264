cc2c84a74bccef1fd9aaa598ca297f001ace76d9
Muhammad Idrees <m.idris991986@gmail.com>
2021-01-23 01:30:04 -0800

    Completed Task 10 of Javascript Assignment 4

---
+++ Task10.js
@@ -0,0 +1,9 @@
+var a = 16.765;
+var b = 12.37982;
+var c = -9.3079499;
+
+document.write("<i>Input<i>"+"  ---------->  "+"<i>Output<i>");
+document.write("<br>");document.write("<br>");
+document.write(a+" ,2"+"  ---------->  "+a.toFixed(2));document.write("<br>");
+document.write(b+" ,2"+"  ---------->  "+b.toFixed(2));document.write("<br>");
+document.write(c+" ,4"+"  ---------->  "+c.toFixed(4));document.write("<br>");
